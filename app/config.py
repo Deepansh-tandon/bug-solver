398b1ec6df45ef7ed2d148a4f7b5ef147ce45bcd
Deepansh <deepanshtandon7@gmail.com>
2025-06-17 02:30:13 +0530

    Delete app/config.py

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-GEMINI_API_KEY = "AIzaSyArKS5eaCxl-Sjj3QG6otpicZs8-roixQ8"
-# GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")  # optional
-QDRANT_URL = "https://00c8b8de-94de-46f4-91ee-5d8684d7f0f4.europe-west3-0.gcp.cloud.qdrant.io:6333"
-QDRANT_API_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.JMLMwRzc51-Iylp0U24vmgqN_JWZqDSqL_Zhpzc4j2c"
-SIMILARITY_THRESHOLD = 0.75
